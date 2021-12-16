a00d8a2f9a819610ce722e5fc23d4080d2b84156
Gleavero <gleavero@gmail.com>
2021-12-16 22:41:47 +0400

    Variance calcualte

--- adaptiveFilter.m
+++ adaptiveFilter.m
@@ -2,8 +2,8 @@ clear all;
 
 t = 1:1000;
 discreteSize = 10;
-rI = 220;
-pI = 170;
+rI = 95;
+pI = 120;
 impulse = generateImpulse(t,100);
 
 noise = generateNoise(t);
@@ -31,6 +31,18 @@ x = filt(noise,additiveMix,discreteSize,rI,ha1);
 
 x1 = filt(noise,additiveMix,discreteSize,pI,ha2);
 
+normX = x.^2;
+normX1 = x1.^2;
+
+diff = pI-rI;
+meanX = smoothMean(normX(pI:pI+diff));
+meanX1 = smoothMean(normX1(pI:pI+diff));
+meanX_X1 = smoothMean(normX(pI:pI+diff)+normX1(pI:pI+diff));
+mean = mean(meanX);
+D = var(meanX);
+D1 = var(meanX1);
+D_D1 = var(meanX_X1);
+
 figure
 subplot(5,1,1); plot(t, impulse); axis([0 length(additiveMix) min(additiveMix) max(additiveMix)]);
 % xline(rI*discreteSize); xline(rI*discreteSize+discreteSize); xline(pI*discreteSize); xline(pI*discreteSize+discreteSize);
@@ -41,22 +53,18 @@ xlabel('Time'); ylabel('Noise');
 subplot(5,1,3); plot(t, additiveMix); axis([0 length(additiveMix) min(additiveMix) max(additiveMix)]);
 xlabel('Time'); ylabel('Signal+Noise');
 subplot(5,1,4); plot(t, x); axis([0 length(additiveMix) min(additiveMix) max(additiveMix)]);
-xlabel('Time'); ylabel('Adaptive Filter 1');
+xline(pI); xline(pI+diff);
+xlabel('Time'); ylabel('Adaptive Filter 1'); 
 subplot(5,1,5); plot(t, x1); axis([0 length(additiveMix) min(additiveMix) max(additiveMix)]);
+xline(pI); xline(pI+diff);
 xlabel('Time'); ylabel('Adaptive Filter 2');
 
-normX = x.^2;
-normX1 = x1.^2;
-meanX = smoothMean(normX(120:1000)+normX1(120:1000));
-meanX1 = smoothMean(normX1);
-mean = mean(meanX);
-D = var(meanX);
 figure
-subplot(4,1,1); plot(t(120:1000),meanX);
+subplot(4,1,1); plot(t(pI:pI+(pI-rI)),meanX);
 % subplot(4,1,2); plot(t,meanX1);
 % subplot(4,1,3); plot (t, meanX-meanX1);
 % subplot(4,1,4); spectrogram(meanX-meanX1);
 Enorm = sum(normX);
 Enorm1 = sum(normX1);
-E = sum(meanX-meanX1);
+% E = sum(meanX-meanX1);
 Eimp = sum(impulse);
