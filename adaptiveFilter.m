ecab9d084ca569f891f9c69ccaf51213f64d93ab
Gleavero <gleavero@gmail.com>
2021-12-21 22:57:38 +0400

    setting model

--- adaptiveFilter.m
+++ adaptiveFilter.m
@@ -1,9 +1,9 @@
 clear all;
 
 t = 1:1000;
-discreteSize = 10;
-rI = 95;
-pI = 120;
+discreteSize = 40;
+rI = 60;
+pI = 160;
 impulse = generateImpulse(t,100);
 
 noise = generateNoise(t);
@@ -11,7 +11,7 @@ noise = generateNoise(t);
 additiveMix = impulse+noise;
 
 %Длина адаптивнго фильтра
-L = 64;
+L = 40;
 %Коэффициент забывания
 lam = 1;
 %Оценка дисперсии входного сигнала
@@ -31,8 +31,11 @@ x = filt(noise,additiveMix,discreteSize,rI,ha1);
 
 x1 = filt(noise,additiveMix,discreteSize,pI,ha2);
 
-normX = x.^2;
-normX1 = x1.^2;
+normX = x(pI+discreteSize:length(t))./norm(x(pI+discreteSize:length(t)));
+normX1 = x1(pI+discreteSize:length(t))./norm(x1(pI+discreteSize:length(t)));
+
+% diffX = normX.^2;
+% diffX1 = normX1.^2;
 
 diff = pI-rI;
 meanX = smoothMean(normX(pI:pI+diff));
@@ -44,26 +47,32 @@ D1 = var(meanX1);
 D_D1 = var(meanX_X1);
 
 figure
-subplot(5,1,1); plot(t, impulse); axis([0 length(additiveMix) min(additiveMix) max(additiveMix)]);
+subplot(5,1,1); plot(t, impulse,[rI rI+discreteSize; rI rI+discreteSize],[min(additiveMix) min(additiveMix);max(additiveMix) max(additiveMix)],'r--', [pI pI+discreteSize; pI pI+discreteSize],[min(additiveMix) min(additiveMix);max(additiveMix) max(additiveMix)],'g--'); 
+axis([0 length(additiveMix) min(additiveMix) max(additiveMix)]);
 % xline(rI*discreteSize); xline(rI*discreteSize+discreteSize); xline(pI*discreteSize); xline(pI*discreteSize+discreteSize);
-xline(rI); xline(rI+discreteSize); xline(pI); xline(pI+discreteSize);
+% xline(rI); xline(rI+discreteSize); xline(pI); xline(pI+discreteSize);
 xlabel('Time'); ylabel('Signal');
 subplot(5,1,2); plot(t, noise); axis([0 length(additiveMix) min(additiveMix) max(additiveMix)]);
 xlabel('Time'); ylabel('Noise');
 subplot(5,1,3); plot(t, additiveMix); axis([0 length(additiveMix) min(additiveMix) max(additiveMix)]);
 xlabel('Time'); ylabel('Signal+Noise');
 subplot(5,1,4); plot(t, x); axis([0 length(additiveMix) min(additiveMix) max(additiveMix)]);
-xline(pI); xline(pI+diff);
+% xline(pI); xline(pI+diff);
 xlabel('Time'); ylabel('Adaptive Filter 1'); 
 subplot(5,1,5); plot(t, x1); axis([0 length(additiveMix) min(additiveMix) max(additiveMix)]);
-xline(pI); xline(pI+diff);
+% xline(pI); xline(pI+diff);
 xlabel('Time'); ylabel('Adaptive Filter 2');
 
+iter = length(normX)-discreteSize;
+
+difference = (normX-normX1).^2;
+meanDiff = movmean(difference, 100);
 figure
-subplot(4,1,1); plot(t(pI:pI+(pI-rI)),meanX);
-% subplot(4,1,2); plot(t,meanX1);
-% subplot(4,1,3); plot (t, meanX-meanX1);
-% subplot(4,1,4); spectrogram(meanX-meanX1);
+subplot(1,1,1); plot(1:length(meanDiff),meanDiff); 
+axis([1 length(meanDiff) 0 0.01])
+
+sumX = sum(difference)/length(difference);
+
 Enorm = sum(normX);
 Enorm1 = sum(normX1);
 % E = sum(meanX-meanX1);
--- generateNoise.m
+++ generateNoise.m
@@ -1,5 +1,5 @@
 function out=generateNoise(t)
-    gaus = wgn(1,1000,0);
+    gaus = wgn(1,length(t),0);
     mainNoise = 2*sin(2*pi*0.055*t);
     
     out = mainNoise+gaus;
